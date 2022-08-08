628d2bb70f8139afc9d6b30abf398448bcad5602
aldvs <andrewgogol777@gmail.com>
2022-08-08 21:43:50 +0300

    1. Update main.swift with task1

--- IWTechnicalTask/main.playground/Contents.swift
+++ IWTechnicalTask/main.playground/Contents.swift
@@ -1,11 +1,4 @@
-//
-//  main.swift
-//  IWTechnicalTask
-//
-//  Created by admin on 08.08.2022.
-//
-
-import Foundation
+import Cocoa
 
 //MARK: - Задание 1
 
@@ -14,25 +7,24 @@ var arrayOfNums = [1,2,3,4,5]
 func getReverseArr(from incomingArray: [Int]) -> [Int] {
     var resultArray = incomingArray
     let count = resultArray.count
-    var temp: Int
     
+    var temp: Int
     var i = 0
+    
     repeat {
         temp = resultArray[i]
         resultArray[i] = resultArray[count - i - 1]
         resultArray[count - i - 1] = temp
         
         i += 1
+        
     } while i < (count / 2)
+    
     return resultArray
 }
-print("Исходный массив: \(arrayOfNums)")
+
 var testArray = getReverseArr(from: arrayOfNums)
-print("Результирующий массив: \(testArray)")
-
-//MARK: - Задание 2
-
-
+print(testArray)
 
 
 
