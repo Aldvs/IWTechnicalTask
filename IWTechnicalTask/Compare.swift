ef941dfe9969203ab993a429f553f833f5ebe562
aldvs <andrewgogol777@gmail.com>
2022-08-24 02:51:03 +0300

    Update naming

--- IWTechnicalTask/Compare.swift
+++ IWTechnicalTask/Compare.swift
@@ -18,45 +18,45 @@ func compareVersions(_ firstString: String?,_ secondString: String?) throws ->  
     var digits = NSCharacterSet.decimalDigits
     digits.insert(charactersIn: ".")
     
-    guard var firstVersion = firstString, !firstVersion.isEmpty else {
+    guard var first = firstString, !first.isEmpty else {
         throw VersionError.emptyFirstVersion
     }
-    guard CharacterSet(charactersIn: firstVersion).isSubset(of: digits) else {
+    guard CharacterSet(charactersIn: first).isSubset(of: digits) else {
         throw VersionError.invalidFirstVersion
     }
-    guard var secondVersion = secondString, !secondVersion.isEmpty else {
+    guard var second = secondString, !second.isEmpty else {
         throw VersionError.emptySecondVersion
     }
-    guard CharacterSet(charactersIn: secondVersion).isSubset(of: digits) else {
+    guard CharacterSet(charactersIn: second).isSubset(of: digits) else {
         throw VersionError.invalidSecondVersion
     }
     
-    if firstVersion.last == "0" { firstVersion.removeLast() }
-    if secondVersion.last == "0" { secondVersion.removeLast() }
-    if firstVersion.filter({ $0 == "."}).count > secondVersion.filter({ $0 == "."}).count {
-        for _ in 0..<(firstVersion.filter({ $0 == "."}).count - secondVersion.filter({ $0 == "."}).count) {
-            secondVersion += "." }
+    if first.last == "0" { first.removeLast() }
+    if second.last == "0" { second.removeLast() }
+    if first.filter({ $0 == "."}).count > second.filter({ $0 == "."}).count {
+        for _ in 0..<(first.filter({ $0 == "."}).count - second.filter({ $0 == "."}).count) {
+            second += "." }
     } else {
-        for _ in 0..<(secondVersion.filter({ $0 == "."}).count - firstVersion.filter({ $0 == "."}).count) {
-            firstVersion += "." } }
+        for _ in 0..<(second.filter({ $0 == "."}).count - first.filter({ $0 == "."}).count) {
+            first += "." } }
     
     var i = 0, j = 0
     repeat {
-        if i >= firstVersion.count { return j >= secondVersion.count ? .orderedSame : .orderedAscending }
-        if j >= secondVersion.count { return .orderedDescending }
-        if firstVersion[firstVersion.index(firstVersion.startIndex, offsetBy: i)] == "0" && secondVersion[secondVersion.index(secondVersion.startIndex, offsetBy: j)] == "0" {
+        if i >= first.count { return j >= second.count ? .orderedSame : .orderedAscending }
+        if j >= second.count { return .orderedDescending }
+        if first[first.index(first.startIndex, offsetBy: i)] == "0" && second[second.index(second.startIndex, offsetBy: j)] == "0" {
             i += 1; j += 1
             continue
-        } else if firstVersion[firstVersion.index(firstVersion.startIndex, offsetBy: i)] == "0" {
+        } else if first[first.index(first.startIndex, offsetBy: i)] == "0" {
             i += 1
             continue
-        } else if secondVersion[secondVersion.index(secondVersion.startIndex, offsetBy: j)] == "0" {
+        } else if second[second.index(second.startIndex, offsetBy: j)] == "0" {
             j += 1
             continue
         }
-        if firstVersion[firstVersion.index(firstVersion.startIndex, offsetBy: i)] < secondVersion[secondVersion.index(secondVersion.startIndex, offsetBy: j)] {
+        if first[first.index(first.startIndex, offsetBy: i)] < second[second.index(second.startIndex, offsetBy: j)] {
             return .orderedAscending
-        } else if firstVersion[firstVersion.index(firstVersion.startIndex, offsetBy: i)] > secondVersion[secondVersion.index(secondVersion.startIndex, offsetBy: j)] {
+        } else if first[first.index(first.startIndex, offsetBy: i)] > second[second.index(second.startIndex, offsetBy: j)] {
             return .orderedDescending
         } else {
             i += 1; j += 1
