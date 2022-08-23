3649f3b2e98d70491640cd882b094ae868a23460
aldvs <andrewgogol777@gmail.com>
2022-08-23 13:24:36 +0300

    Fixed character validating in compare swift

--- IWTechnicalTask/Compare.swift
+++ IWTechnicalTask/Compare.swift
@@ -15,25 +15,23 @@ enum VersionError: Error {
 
 func compareVersions(compare firstVersion: String?, and secondVersion: String?) throws ->  ComparisonResult {
     
-    let set: Set = ["0","1","2","3","4","5","6","7","8","9","."]
+    var digits = NSCharacterSet.decimalDigits
+    digits.insert(charactersIn: ".")
     
     guard let firstString = firstVersion, !firstString.isEmpty else {
         throw VersionError.emptyFirstVersion
     }
-    for char in firstString {
-        guard set.contains(String(char)) else {
-            throw VersionError.invalidFirstCharacters
-        }
+    guard CharacterSet(charactersIn: firstString).isSubset(of: digits) else {
+        throw VersionError.invalidFirstCharacters
     }
+    
     guard let secondString = secondVersion, !secondString.isEmpty else {
         throw VersionError.emptySecondVersion
     }
-    for char in secondString {
-        guard set.contains(String(char)) else {
-            throw VersionError.invalidSecondCharacters
-        }
+    guard CharacterSet(charactersIn: secondString).isSubset(of: digits) else {
+        throw VersionError.invalidSecondCharacters
     }
-    
+
     var i = 0, j = 0
     repeat {
         if i >= firstString.count { return j >= secondString.count ? .orderedSame : .orderedAscending }
