ada6e73a7be168d73ae6e453ebe381fbd84d500d
aldvs <andrewgogol777@gmail.com>
2022-09-07 12:51:25 +0300

    update algorithm

--- IWTechnicalTask/Compare.swift
+++ IWTechnicalTask/Compare.swift
@@ -16,6 +16,7 @@ enum VersionError: Error {
 func compareVersions(_ firstVersion: String,_ secondVersion: String) throws ->  ComparisonResult {
     var first = firstVersion
     var second = secondVersion
+    var result: ComparisonResult = .orderedSame
     var digits = NSCharacterSet.decimalDigits
     digits.insert(charactersIn: ".")
     
@@ -27,25 +28,22 @@ func compareVersions(_ firstVersion: String,_ secondVersion: String) throws ->  
     }
     
     if first.filter({ $0 == "."}).count > second.filter({ $0 == "."}).count {
-        for _ in 0..<(first.filter({ $0 == "."}).count - second.filter({ $0 == "."}).count) {
-            second += "." }
+        second += String(repeating: ".", count: first.filter({ $0 == "."}).count - second.filter({ $0 == "."}).count)
     } else {
-        for _ in 0..<(second.filter({ $0 == "."}).count - first.filter({ $0 == "."}).count) {
-            first += "." } }
-    
+        first += String(repeating: ".", count: second.filter({ $0 == "."}).count - first.filter({ $0 == "."}).count)
+    }
     if (first.last?.isNumber != nil) || (second.last?.isNumber != nil) {
         first.append(contentsOf: ".")
         second.append(contentsOf: ".")
     }
-    
     var i = 0, j = 0
-    repeat {
-        if i >= first.count { return j >= second.count ? .orderedSame : .orderedAscending }
-        if j >= second.count { return .orderedDescending }
+    for _ in 0...max(first.count, second.count) - 1  {
+        if i >= first.count { j >= second.count ? (result = .orderedSame) : (result = .orderedAscending) }
+        if j >= second.count { result = .orderedDescending }
         if first[first.index(first.startIndex, offsetBy: i)] == "0" && second[second.index(second.startIndex, offsetBy: j)] == "0" {
             i += 1; j += 1
-            continue
-        } else if first[first.index(first.startIndex, offsetBy: i)] == "0" {
+            continue } else
+        if first[first.index(first.startIndex, offsetBy: i)] == "0" {
             i += 1
             continue
         } else if second[second.index(second.startIndex, offsetBy: j)] == "0" {
@@ -53,12 +51,13 @@ func compareVersions(_ firstVersion: String,_ secondVersion: String) throws ->  
             continue
         }
         if first[first.index(first.startIndex, offsetBy: i)] < second[second.index(second.startIndex, offsetBy: j)] {
-            return .orderedAscending
+            result = .orderedAscending
         } else if first[first.index(first.startIndex, offsetBy: i)] > second[second.index(second.startIndex, offsetBy: j)] {
-            return .orderedDescending
+            result = .orderedDescending
         } else {
             i += 1; j += 1
             continue
         }
-    } while true
+    }
+    return result
 }
